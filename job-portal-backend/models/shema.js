d6b2aba738f26146a775deaa60cf1aad759ec6f3
Nirjal <tiwarinirjal0@gmail.com>
2026-05-29 15:47:08 +0700

    reconstructed folder structure using bash script

--- job-portal-backend/models/shema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userInformationSchema = new mongoose.Schema({
-    name : {type : String, required : true},
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-},{collection:"userInformation"})
-
-export default mongoose.model("userInformation", userInformationSchema)
---
+++ job-portal-backend/src/routes/auth.route.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { register, login, getUser, updateUser } from '../controllers/auth.controller.js';
+import { auth, verifyUser, localVariables } from '../middleware/auth.middleware.js';
+
+const router = Router();
+
+router.post('/register', register);
+router.post('/login', verifyUser, login);
+router.get('/user/:username', auth, getUser);
+router.put('/update', auth, updateUser);
+
+export default router;
